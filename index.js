f4da1e6c31fb6d631714a7d956839243034521e8
Matubo <Matbei14@yandex.ru>
2020-05-28 14:04:02 +0300

    start my_carousel

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ window.onload=function() {
 });
 },500); 
 };
+
+let carusel_items=document.getElementsByClassName("my_carousel_img");
+
+function my_carusel_next(){
+carusel_items[0].style.cssText=`
+transform:translateX(0,0)
+
+`
+}
+function my_carusel_previous(){
+    console.log("next");
+    }
