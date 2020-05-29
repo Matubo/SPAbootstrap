a8538b4ca2735349eeb0f7022991faae03ddc3d0
Matubo <Matbei14@yandex.ru>
2020-05-29 15:50:46 +0300

    opacity

--- index.js
+++ index.js
@@ -35,36 +35,43 @@ function my_carusel_transform(){
     carusel_items[carusel_arr_marker[0]].style.cssText=`
     transform:translate(-95%,-50%);
     height: 40vh;
+    opacity: 0.7;
     z-index:1097;
     `;
     carusel_items[carusel_arr_marker[1]].style.cssText=`
     transform:translate(-85%,-50%);
     height: 43vh;
+    opacity: 0.8;
     z-index:1098;
     `;
     carusel_items[carusel_arr_marker[2]].style.cssText=`
     transform:translate(-70%,-50%);
     height: 45vh;
+    opacity: 0.9;
     z-index:1099;
     `;
     carusel_items[carusel_arr_marker[3]].style.cssText=`
     transform:translate(-50%,-50%);
     height: 50vh;
+    opacity: 1;
     z-index:1100;
     `;
     carusel_items[carusel_arr_marker[4]].style.cssText=`
     transform:translate(-30%,-50%);
     height: 45vh;
+    opacity: 0.9;
     z-index:1099;
     `;
     carusel_items[carusel_arr_marker[5]].style.cssText=`
     transform:translate(-15%,-50%);
     height: 43vh;
+    opacity: 0.8;
     z-index:1098;
     `;
     carusel_items[carusel_arr_marker[6]].style.cssText=`
     transform:translate(-5%,-50%);
     height: 40vh;
+    opacity: 0.7;
     z-index:1097;
     `;
 }
